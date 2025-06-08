447e04bf6a46842d341d4a9bf56c39a1637ec418
Manti <mokonemanti256@gmail.com>
2025-06-08 08:42:26 -0400

    Views made

--- server/backend/api/urls.py
+++ server/backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
 from django.http import HttpResponse
-from api.views import api_home
+from api.views import api_home, PredictTextAPIView
 
 urlpatterns = [
     path('',api_home ),
+    path('predict/', PredictTextAPIView.as_view(), name='predict'),
 ]
